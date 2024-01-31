e957b6fe754c4f38f81c3731c8619ed6aef1a25e
ImagineMan <setonhallrobotics@gmail.com>
2024-01-31 16:11:50 -0500

    thirdtest

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 //this is a test for the github!
 // this is a second test for github sharing
+// third test
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
