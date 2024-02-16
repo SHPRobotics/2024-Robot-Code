829fb3a22a1fa8749a7bcf59cab1dc35d1c24cbf
SHPRobotics <setonhallrobotics@gmail.com>
2024-02-16 10:54:47 -0500

    Revised Ground Intake Subsystem

--- src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
@@ -3,11 +3,9 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkBase.SoftLimitDirection;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.InclinerConstants;
 import frc.robot.Constants.GroundIntakeConstants;
 //creates new ground subsystem
 public class GroundIntakeSubsystem extends SubsystemBase {
